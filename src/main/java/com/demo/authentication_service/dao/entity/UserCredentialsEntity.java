4db7e10500f7a8fffc5617436ecafdb1b2c0cc3a
VishnuCBalaraman <vishnucbalaram123@gmail.com>
2024-10-05 11:37:55 +0530

    first-commit

---
+++ src/main/java/com/demo/authentication_service/dao/entity/UserCredentialsEntity.java
@@ -0,0 +1,65 @@
+package com.demo.authentication_service.dao.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@Data
+
+@Entity
+public class UserCredentialsEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private String email;
+	private String password;
+
+	public UserCredentialsEntity() {
+	}
+
+	public UserCredentialsEntity(int id, String name, String email, String password) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.password = password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
